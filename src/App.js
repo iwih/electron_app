6a6292349956d7c79386995d005e7a2337e4c527
iwih <osama.a.shammari@gmail..com>
2018-09-09 23:00:41 +0300

    data is retrieved from reddit.com and titles are displayed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import {Icon} from "semantic-ui-react"
-import "semantic-ui-css/semantic.min.css"
+import "./App.css"
+import axios from 'axios'
 
+class App extends React.Component {
+    state = {
+        posts: []
+    }
 
-let style = {
-  justifyContent: 'center',
-  alignItems: 'center',
-  fontSize: 20
-  }
-const App = () => (
-      <div className="App" style={style}>
-      This div contains semantic-ui-react Icon, loading of name ="add user".
-        <Icon loading name="google" size="huge"/>
-      </div>
-    );
+    componentDidMount() {
+        axios.get("https://www.reddit.com/r/aww.json").then(response => {
+            this.setState({posts: response.data.data.children})
+        }).catch(error => {
+            console.log(error)
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <ul className={"list-group list-group-flush"}>
+                    {
+                        this.state.posts.map(
+                            post => <li className={"list-group-item"} key={post.data.id}>{post.data.title}</li>)
+                    }
+                </ul>
+            </div>
+
+        );
+    }
+}
 
 export default App;
