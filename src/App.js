e11ae465d3c4272320661ded591e6fa19891693c
Osama Al Shammari <iwih@outlook.com>
2018-09-09 21:27:50 +0300

    playing first games!

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Icon} from "semantic-ui-react"
+import "semantic-ui-css/semantic.min.css"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+let style = {
+  justifyContent: 'center',
+  alignItems: 'center',
+  fontSize: 20
+  }
+const App = () => (
+      <div className="App" style={style}>
+      This div contains semantic-ui-react Icon, loading of name ="add user".
+        <Icon loading name="google" size="huge"/>
       </div>
     );
-  }
-}
 
 export default App;
